7ccd309769eab4f3cf86672aed31a51c3800d038
zkrx <zkrx>
2022-08-07 19:04:21 +0200

    hw: ide: mmio: use native endian instead of little endian for MMIO registers
    This was changed from native to little in [1]. However, our big-endian Tektronix
    device accesses these in big-endian. Revert the change from [1].
    
    [1] https://patchwork.ozlabs.org/project/qemu-devel/patch/5402DD88.5010600@gmail.com/

--- hw/ide/mmio.c
+++ hw/ide/mmio.c
@@ -88,7 +88,7 @@ static void mmio_ide_write(void *opaque, hwaddr addr,
 static const MemoryRegionOps mmio_ide_ops = {
     .read = mmio_ide_read,
     .write = mmio_ide_write,
-    .endianness = DEVICE_LITTLE_ENDIAN,
+    .endianness = DEVICE_NATIVE_ENDIAN,
 };
 
 static uint64_t mmio_ide_status_read(void *opaque, hwaddr addr,
@@ -112,7 +112,7 @@ static void mmio_ide_ctrl_write(void *opaque, hwaddr addr,
 static const MemoryRegionOps mmio_ide_cs_ops = {
     .read = mmio_ide_status_read,
     .write = mmio_ide_ctrl_write,
-    .endianness = DEVICE_LITTLE_ENDIAN,
+    .endianness = DEVICE_NATIVE_ENDIAN,
 };
 
 static const VMStateDescription vmstate_ide_mmio = {
